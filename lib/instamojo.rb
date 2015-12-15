6c1f1f3512ed4348294dcd83652b88e38e9fa103
Anmol Agrawal <anmol.agrawal@icicletech.com>
2015-12-15 16:36:02 +0530

    added links

--- lib/instamojo.rb
+++ lib/instamojo.rb
@@ -1,5 +1,10 @@
 # http://guides.rubygems.org/make-your-own-gem/#your-first-gem
-class Instamojo
+# https://blog.engineyard.com/2014/wrapping-your-api-in-a-ruby-gem
+# http://guides.rubygems.org/rubygems-org-api/
+# Integrating_Instamojo_v1_2.pdf
+# https://github.com/Instamojo/instamojo-py/blob/master/instamojo/api.py
+# https://www.instamojo.com/developers/rest/
+class Instamojo(object)
   # def self.hi
   #   puts "Hello world!"
   # end

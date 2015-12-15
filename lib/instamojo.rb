881e4effbe8e955a4de8608585b31b7d7e0ce01b
Anmol Agrawal <anmol.agrawal@icicletech.com>
2015-12-15 19:03:35 +0530

    added create auth token

--- lib/instamojo.rb
+++ lib/instamojo.rb
@@ -4,19 +4,29 @@
 # Integrating_Instamojo_v1_2.pdf
 # https://github.com/Instamojo/instamojo-py/blob/master/instamojo/api.py
 # https://www.instamojo.com/developers/rest/
+# https://github.com/jnunemaker/httparty
+# http://blog.teamtreehouse.com/its-time-to-httparty
 require 'httparty'
 
 class Instamojo
 
-  @headers = { "X-Api-Key" => "cbd5e5bdfa7227960d1e4170cca8308c", "X-Auth-Token" => "cf6c7f98831144957e1913f40bd1c1a7" }
+  @api_key = "cbd5e5bdfa7227960d1e4170cca8308c"
+  @auth_token = "cf6c7f98831144957e1913f40bd1c1a7"
+
+  @headers = { "X-Api-Key" => @api_key, "X-Auth-Token" => @auth_token }
 
   def self.debug
     response = HTTParty.get('https://www.instamojo.com/api/1.1/debug/', :headers => @headers)
     puts response.body
   end
 
-  def self.hi
-    puts "hello world"
+  def self.create_auth_token
+    response = HTTParty.post('https://www.instamojo.com/api/1.1/tokens/', :headers => { "X-Api-Key" => @api_key})
+    puts response.body
   end
+
+  # def self.hi
+  #   puts "hello world"
+  # end
 
 end

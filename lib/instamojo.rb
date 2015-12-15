7d761868720fcc7a15ee009684ed1a91a7a460fa
Anmol Agrawal <anmol1771@gmail.com>
2015-12-16 02:17:58 +0530

    just added all the methods

--- lib/instamojo.rb
+++ lib/instamojo.rb
@@ -20,11 +20,91 @@ class Instamojo
     puts response.body
   end
 
+  # Authentication
+
   def self.create_auth_token
     response = HTTParty.post('https://www.instamojo.com/api/1.1/tokens/', :headers => { "X-Api-Key" => @api_key})
     puts response.body
   end
 
+  def self.list_auth_tokens
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/tokens/', :headers => @headers)
+    puts response.body
+  end
+
+  def self.delete_auth_token(auth_token)
+    response = HTTParty.delete('https://www.instamojo.com/api/1.1/tokens/#{auth_token}', :headers => @headers)
+    puts response.body
+  end
+
+  # Link
+
+  def self.get_all_links
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/links/', :headers => @headers)
+    puts response.body
+  end
+
+  # http://stackoverflow.com/questions/19461333/how-can-i-implement-this-post-request-using-httparty
+  def self.new_link
+    response = HTTParty.post('https://www.instamojo.com/api/1.1/links/', :headers => @headers,
+      :body => [{ "title" => "sample title", "description" => "sample description", "currency" => "INR", "base_price" => "10.00"}].to_json
+    )
+    puts response.body
+  end
+
+  def self.get_file_upload_url
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/links/get_file_upload_url/', :headers => @headers)
+    puts response.body
+  end
+
+  def self.upload_file # based on above link
+    # https://github.com/jwagener/httmultiparty
+  end
+
+  def self.link_details(slug)
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/links/#{slug}', :headers => @headers)
+    puts response.body
+  end
+
+  def self.archive_link
+
+  end
+
+  # Payments
+
+  def self.list_all_payments
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/payments/', :headers => @headers)
+    puts response.body
+  end
+
+  # Filter List of All Payments by Date
+  def method_name
+
+  end
+
+  def self.payment_details(payment_id)
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/payments/#{payment_id}/', :headers => @headers)
+    puts response.body
+  end
+
+  # Refunds
+
+  def self.new_refund
+    response = HTTParty.post('https://www.instamojo.com/api/1.1/refunds/', :headers => @headers,
+      :body => [{ "payment_id" => "1234", "type" => "RFD", "body" => "refunded due to duplicate payment"}].to_json
+    )
+    puts response.body
+  end
+
+  def self.refunds_list
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/refunds/', :headers => @headers)
+    puts response.body
+  end
+
+  def self.refund_details(refund_id)
+    response = HTTParty.get('https://www.instamojo.com/api/1.1/refunds/#{refund_id}', :headers => @headers)
+    puts response.body
+  end
   # def self.hi
   #   puts "hello world"
   # end
